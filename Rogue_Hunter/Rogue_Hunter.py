60110272608a12735ee1c8d9bb21d4d690412b29
Antonio Jarreta <antoniojarreta@gmail.com>
2017-09-06 01:20:26 +0200

    First version of DrawingGrid function

--- Rogue_Hunter/Rogue_Hunter.py
+++ Rogue_Hunter/Rogue_Hunter.py
@@ -5,13 +5,74 @@ import pygame
 from pygame.locals import *
 import win32api
 
+# GENERAL VARIABLES
+WindowDimensions = [0, 0]
+GridDimensions = [0, 0]
+PlayingGrid = []
+RedPlayerStart = [9, 4]
+BluePlayerStart = [9, 15 ]
+
 # FUNCTIONS DEFINITIONS
-def GridConstructor(InputGrid):
+def GridConstructor(InputGrid, RedPosition, BluePosition):
+    PositionMarker = [0, 0]
     for x in range(20):
+        PositionMarker[0] = x
         GridRow = []
-        for x in range(19):
-            GridRow.append(0)
-        InputGrid.append(GridRow)
+        for y in range(19):
+            PositionMarker[1] = y
+            if PositionMarker == RedPosition:
+                GridRow.append(1)
+            elif PositionMarker == BluePosition:
+                GridRow.append(2)
+            else:
+                GridRow.append(0)
+            InputGrid.append(GridRow)
+
+def GridUpdate(InputGrid, RedPosition, BluePosition):
+    PositionMarker = [0, 0]
+    for x in len(InputGrid[x]):
+        PositionMarker[0] = x
+        GridRow = []
+        for y in range(19):
+            PositionMarker[1] = y
+            if PositionMarker == RedPosition:
+                GridRow[x][y] = 1
+            elif PositionMarker == BluePosition:
+                GridRow[x][y] = 2
+            else:
+                GridRow[x][y] = 0
+
+def DrawingGrid(InputGrid, Surface, GridDimensions, WindowDimensions, SquareSize):
+    StartingPositionA = [int((WindowDimensions[0] - GridDimensions[0])/2), int((WindowDimensions[1] - GridDimensions[1])/2)]
+    StartingPositionB = [int(StartingPositionA[0] * 1.05), int(StartingPositionA[1] * 1.05)]
+    CurrentPositionA = StartingPositionA
+    CurrentPositionB = StartingPositionB
+    for x in InputGrid:
+        index = 0
+        for y in InputGrid[index]:
+            if y == 0:
+                pygame.draw.rect(Surface, BaseBlack, (CurrentPositionA[0], CurrentPositionA[1], SquareSize, SquareSize), 0)
+                pygame.draw.rect(Surface, BaseWhite, (CurrentPositionB[0], CurrentPositionB[1], int(SquareSize * 0.9), int(SquareSize * 0.9)), 0)
+                CurrentPositionA[1] += SquareSize
+                CurrentPositionB[1] += SquareSize
+                print InputGrid[index][y]
+                print StartingPositionA
+                print StartingPositionB
+            elif y == 1:
+                pygame.draw.rect(Surface, BaseBlack, (CurrentPositionA[0], CurrentPositionA[1], SquareSize, SquareSize), 0)
+                pygame.draw.rect(Surface, BaseRed, (CurrentPositionB[0], CurrentPositionB[1], int(SquareSize * 0.9), int(SquareSize * 0.9)), 0)
+                CurrentPositionA[1] += SquareSize
+                CurrentPositionB[1] += SquareSize
+            else:
+                pygame.draw.rect(Surface, BaseBlack, (CurrentPositionA[0], CurrentPositionA[1], SquareSize, SquareSize), 0)
+                pygame.draw.rect(Surface, BaseBlue, (CurrentPositionA[0], CurrentPositionB[1], int(SquareSize * 0.9), int(SquareSize * 0.9)), 0)
+                CurrentPositionA[1] += SquareSize
+                CurrentPositionB[1] += SquareSize
+        CurrentPositionA[0] += SquareSize
+        CurrentPositionA[1] = StartingPositionA
+        CurrentPositionB[0] += SquareSize
+        CurrentPositionB[1] = StartingPositionB
+        index +=1
 
 # COLOURS DEFINITION
 BaseBlack = (0, 0, 0)
@@ -28,22 +89,22 @@ GridHeight = int(ScreenHeight*0.9)
 while (GridHeight % 20) != 0:
     GridHeight -= 1
 GridWidth = int(GridHeight*0.95)
+GridDimensions = [GridWidth, GridWidth]
 BaseSquareSize = GridHeight/20
 
-PlayingGrid = []
-GridConstructor(PlayingGrid)
+'''GridConstructor(PlayingGrid)
 x = 1
 for item in PlayingGrid:
     print x, item, '\n'
-    x += 1
+    x += 1'''
 
 #MAIN LOOP
 pygame.init()
 pygame.display.set_caption('Rogue Hunter')
 while True:
     ROGUE_HUNTERWindow = pygame.display.set_mode(WindowDimensions)
-    #for item
-    pygame.draw.rect(ROGUE_HUNTERWindow, BaseWhite, (BaseSquareSize, BaseSquareSize, BaseSquareSize, BaseSquareSize), 1)
+    GridConstructor(PlayingGrid, RedPlayerStart, BluePlayerStart)
+    DrawingGrid(PlayingGrid, ROGUE_HUNTERWindow, GridDimensions, WindowDimensions, BaseSquareSize)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT:
